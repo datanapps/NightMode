c9c108f8663c623b4788f2b97e53cd86d3111369
Yogendra-mediacorp <yogendra.singh@mediacorp.com.sg>
2019-07-31 15:55:44 +0800

    added screenshot

--- app/src/main/java/com/example/nightmode/MainActivity.java
+++ app/src/main/java/com/example/nightmode/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.nightmode;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
